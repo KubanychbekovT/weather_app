d8504a3355dc49ddba361f78db0c2871dc5bd79b
Temirlan <brucewaynetemir@gmail.com>
2023-06-30 13:01:31 +0600

    refactor: restyle images_page

--- lib/presentation/image/images_page.dart
+++ lib/presentation/image/images_page.dart
@@ -11,22 +11,27 @@ class ImagesPage extends StatefulWidget {
 class _ImagesPageState extends State<ImagesPage> {
   List<ImageData> images = [
     ImageData(
+      title: 'Sunny',
       imageUrl: 'assets/posts/sun.jpg',
       description: 'Sunny or clear means there are no clouds in the sky, and cloudy means the entire sky is covered by clouds. One of the most misused weather terms is "fair." The NWS uses "fair," typically at night, to describe less than 3/8 cloud cover, with no precipitation and no extremes of visibility, temperature or winds.',
       ),
     ImageData(
+      title: 'Rainy',
       imageUrl: 'assets/posts/rain.jpg',
       description: 'Rain is liquid precipitation: water falling from the sky. Raindrops fall to Earth when clouds become saturated, or filled, with water droplets. Millions of water droplets bump into each other as they gather in a cloud. When a small water droplet bumps into a bigger one, it condenses, or combines, with the larger one.',
     ),
     ImageData(
+      title: 'Snowy',
       imageUrl: 'assets/posts/snow.jpg',
       description: 'Snow is precipitation that forms when water vapor freezes. Snow falls as ice crystals from clouds when temperatures drop below freezing and there is enough humidity in the air.',
     ),
     ImageData(
+      title: 'Windy',
       imageUrl: 'assets/posts/wind.jpg',
       description: 'Wind is the movement of air from an area of high pressure to an area of low pressure. In fact, wind exists because the sun unevenly heats the surface of the Earth. As hot air rises, cooler air moves in to fill the void. As long as the sun shines, the wind will blow.',
     ),
     ImageData(
+      title: 'Stormy',
       imageUrl: 'assets/posts/storm.jpg',
       description: 'Storms are raging atmospheric disturbance that is characterized by cloud cover, low barometric pressure, precipitation, and also with wild winds. They are also coupled with thunder and lightning. The cause of the storm is the moisture content in the air.',
     ),
@@ -69,6 +74,15 @@ class _ImagesPageState extends State<ImagesPage> {
                 return Column(
                   children: [
                     SizedBox(height: 16),
+                Text(
+                  imageData.title,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Color(0xff3d4a73),
+                  ),
+                ),
+                    SizedBox(height: 10),
                     Container(
                       width: 300,
                       height: 300,
@@ -85,6 +99,9 @@ class _ImagesPageState extends State<ImagesPage> {
                       style: TextStyle(
                         fontSize: 16,
                         fontWeight: FontWeight.bold,
+                        color: Color(0xff3d4a73),
+                        fontStyle: FontStyle.italic,
+
                       ),
                     ),
                   ],
@@ -100,10 +117,12 @@ class _ImagesPageState extends State<ImagesPage> {
 }
 
 class ImageData {
+  final String title;
   final String imageUrl;
   final String description;
 
   ImageData({
+    required this.title,
     required this.imageUrl,
     required this.description,
   });
